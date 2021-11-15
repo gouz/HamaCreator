0d6e9e13a725d6e66a4cd3fd220930f0c74804a6
Sylvain Gougouzian <sylvain@lidrea.fr>
2021-11-15 23:44:54 +0100

    feat(image manipulation): :construction: number of pegboard

--- src/js/dropped.js
+++ src/js/dropped.js
@@ -13,6 +13,12 @@ img.addEventListener(
   'load',
   () => {
     clearCanvas();
+    const nbX = 1;
+    const nbY = 1;
+    tempCanvas.width = nbX * 29;
+    tempCanvas.height = nbY * 29;
+    canvas.width = nbX * 580;
+    canvas.height = nbY * 580;
     let newWidth = img.width;
     let newHeight = img.height;
     newHeight *= tempCanvas.width / newWidth;
@@ -37,8 +43,14 @@ img.addEventListener(
       for (let y = 0; y < tempCanvas.height; y++) {
         const data = tempContext.getImageData(x, y, 1, 1).data;
         context.beginPath();
-        context.arc(5 + x * 10, 5 + y * 10, 4, 0, 2 * Math.PI);
-        context.lineWidth = 3;
+        context.arc(
+          (10 + x * 20) / nbX,
+          (10 + y * 20) / nbY,
+          8 / nbX,
+          0,
+          2 * Math.PI
+        );
+        context.lineWidth = 5 / nbX;
         context.strokeStyle = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3]})`;
         context.stroke();
       }
