37f461731c5766931faed724da62d410c4df7b04
Sylvain Gougouzian <sylvain@lidrea.fr>
2021-11-23 21:45:21 +0100

    feat(UX): :sparkles: Add click on canvas for mobile

--- src/js/main.js
+++ src/js/main.js
@@ -22,6 +22,8 @@ const $spinner = document.querySelector('#spinner');
 
 const $instructions = document.querySelector('#instructions');
 
+const $loadimg = document.querySelector('#loadimg');
+
 const codes =
   'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+-*/$!?';
 
@@ -227,6 +229,21 @@ const imgTreatment = () => {
   $spinner.style.display = 'none';
 };
 
+const loadFiles = (files) => {
+  if (files.length > 0) {
+    const file = files[0];
+    if (typeof FileReader !== 'undefined' && file.type.indexOf('image') != -1) {
+      $instructions.innerHTML = '';
+      $spinner.style.display = 'block';
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        img.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+};
+
 img.addEventListener(
   'load',
   () => {
@@ -246,23 +263,16 @@ $canvas.addEventListener(
 $canvas.addEventListener(
   'drop',
   (e) => {
-    const files = e.dataTransfer.files;
-    if (files.length > 0) {
-      const file = files[0];
-      if (
-        typeof FileReader !== 'undefined' &&
-        file.type.indexOf('image') != -1
-      ) {
-        $instructions.innerHTML = '';
-        $spinner.style.display = 'block';
-        const reader = new FileReader();
-        reader.onload = (e) => {
-          img.src = e.target.result;
-        };
-        reader.readAsDataURL(file);
-      }
-    }
+    loadFiles(e.dataTransfer.files);
     e.preventDefault();
+  },
+  false
+);
+
+$canvas.addEventListener(
+  'click',
+  (e) => {
+    $loadimg.click();
   },
   false
 );
@@ -281,6 +291,14 @@ $vertical.addEventListener(
   () => {
     nbY = $vertical.value;
     redrawCanvas();
+  },
+  false
+);
+
+$loadimg.addEventListener(
+  'change',
+  (e) => {
+    loadFiles(e.target.files);
   },
   false
 );
